0ff00b29b281080ceb30c698043fe07dee60e328
KoshkinVlad <xenomorph-balash@yandex.ru>
2020-05-31 16:05:40 +0300

    Сделал оба задания ДЗ №3

--- src/main/java/geekbrains/lesson01/Homework.java
+++ src/main/java/geekbrains/lesson01/Homework.java
@@ -1,47 +1,103 @@
 package geekbrains.lesson01;
 
+import java.lang.reflect.Array;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Homework {
     public static void main(String[] args) {
-        int[] arr = new int[]{0, 1, 1, 0, 1, 0, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0)
-                arr[i] = 1;
-            else
-                arr[i] = 0;
-        }
+        GuessTheNumber();
+        WordPicker();
+    }
 
-        int[] arr2 = new int[8];
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = 3 * i;
+    public static void GuessTheNumber() {
+        //        Написать программу, которая загадывает случайное число от 0 до 9, и пользователю дается 3 попытки угадать это число.
+        //        При каждой попытке компьютер должен сообщить больше ли указанное пользователем число чем загаданное, или меньше.
+        //        После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
+        Random rand = new Random();
+        int maxTryCount = 3;
+        int userNumber;
+        int repeat = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Компьютер загадал случайное число от 0 до 9. Попробуй отгадать его. У тебя 3 попытки...");
+            int randomNumber = rand.nextInt(10);
+            boolean isWin = false;
+            for (int currentTry = 1; currentTry <= maxTryCount; currentTry++) {
+                System.out.print("Попытка номер " + currentTry + ".\nВведи число: ");
+                userNumber = sc.nextInt();
+                if (userNumber < randomNumber) {
+                    System.out.println("Введённое число меньше загаданного");
+                } else if (userNumber > randomNumber) {
+                    System.out.println("Введённое число больше загаданного");
+                } else {
+                    System.out.println("Вы угадали число!");
+                    isWin = true;
+                    break;
+                }
+            }
+            System.out.println(isWin ? "Вы победили!" : "Вы проиграли :( Правильное число: " + randomNumber);
+            System.out.print("Повторить игру? 1 - да, 0 - нет : ");
+            repeat = sc.nextInt();
+            if (repeat == 0) break;
         }
+        while (repeat == 1);
+//        sc.close(); если закрыть сканер тут, во 2м методе вызывается исключение java.util.NoSuchElementException
+    }
 
-        int[] arr3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6)
-                arr3[i] *= 2;
+    public static void WordPicker() {
+//        Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+//        При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+//                сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
+//                apple – загаданное
+//        apricot - ответ игрока
+//        ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+//        Для сравнения двух слов посимвольно, можно пользоваться:
+//        String str = "apple";
+//        str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
+//        Играем до тех пор, пока игрок не отгадает слово
+//        Используем только маленькие буквы
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        String rightWord = words[rand.nextInt(words.length)];
+        System.out.println("Компьютер загадал одно из следующих слов: ");
+        for (String s : words) {
+            System.out.print(s + ", ");
         }
-
-        int[][] arr4 = new int[5][5];
-        // поскольку мы имеем дело с диагональными элементами, можно обойтись одним циклом
-        for (int i = 0; i < arr4.length; i++) {
-            arr4[i][i] = 1;
+        System.out.println("\nПопробуй угадать, какое слово он загадал, а компьютер подскажет, какие буквы стоят на своих местах");
+        String userWord;
+        do {
+            System.out.print("Ваше предположение: ");
+            userWord = sc.next().toLowerCase();
+            if (userWord.equals(rightWord)) {
+                System.out.println("Вы угадали!");
+                break;
+            } else {
+                // надо из двух слов найти слово с минимальным количеством символов
+                // чтобы в цикле не вылезти за пределы слова
+                int min = userWord.length();
+                if (min > rightWord.length()) {
+                    min = rightWord.length();
+                }
+                String prompt = ""; // тут будет подсказка
+                for (int i = 0; i < min; i++) {
+                    if (rightWord.charAt(i) == userWord.charAt(i)) {
+                        prompt += userWord.charAt(i);
+                    } else {
+                        prompt += "#";
+                    }
+                }
+                // заполняем подсказку до 15 символов
+                int symbolAmount = 15;
+                for (int i = prompt.length(); i < symbolAmount; i++) {
+                    prompt += "#";
+                }
+                System.out.println("Открыты буквы, если они есть: " + prompt);
+            }
         }
-
-        int[] arr5 = new int[10];
-        Random r = new Random(); // чтобы заполнить массив случайными числами
-        for (int i = 0; i < arr5.length; i++) {
-            arr5[i] = r.nextInt();
-        }
-        int min = arr5[0];
-        int max = arr5[0];
-        for (int i = 0; i < arr5.length; i++) {
-            if (arr5[i] < min)
-                min = arr5[i];
-            if (arr5[i] > max)
-                max = arr5[i];
-        }
+        while (true);
+        sc.close();
     }
 
 }
